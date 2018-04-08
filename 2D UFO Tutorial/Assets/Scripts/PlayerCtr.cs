e2c46caf62c5392fec2390afa7cc48982d15c259
uc10d <124073651@qq.com>
2018-04-08 10:08:47 +0800

    2d UFO

---
+++ 2D UFO Tutorial/Assets/Scripts/PlayerCtr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtr : MonoBehaviour {
+
+	public float speech;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+
+	void FixedUpdate()
+	{	
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector2 v = new Vector2 (moveHorizontal, moveVertical);
+		rb.AddForce(v * speech);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		print ("-------------------------------");
+		
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{	
+			
+			other.gameObject.SetActive (false);
+		}
+			
+	}
+	
+
+}
---
+++ 2D UFO Tutorial/UFO 2D.md
@@ -0,0 +1,15 @@
+# UFO 2D
+
+* 摄像机
+* layout层级
+* Sprite
+* Rigidbody2D
+* Collider
+
+
+2d游戏总结
+
+* 场景
+
+
+
